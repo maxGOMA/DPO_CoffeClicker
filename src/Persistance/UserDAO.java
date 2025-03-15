e60373879b308be83f40c45a656fb6269cdd56fa
alexiaJulia <alexia.julia@students.salle.url.edu>
2025-03-15 14:15:28 +0100

    dataBaseUsers implementat

--- src/Persistance/UserDAO.java
+++ src/Persistance/UserDAO.java
@@ -5,6 +5,8 @@ package Persistance;
 
 import Business.Entities.EntityUser;
 
+import Persistance.PersistanceException;
+
 /**
  * Interface that abstracts the persistence of groups from the rest of the code.
  * In particular, it follows the Data Access Object design pattern, which is commonly used to abstract persistence
@@ -15,26 +17,26 @@ public interface UserDAO {
     /**
      * Method that saves a user in the system, persisting it.
      *
-     * @param userName The new user_name to save.
+     * @param username The new user_name to save.
      * @param password The password from the user to save.
      * @param email The email from the user to save.
      */
-    void registerUser(String userName, String password, String email);
+    void registerUser(String username, String password, String email);
 
     /**
      * Method that reads the persisted information, returning all stored courses.
      *
-     * @param userName user_name from the user to delete.
+     * @param username user_name from the user to delete.
      */
-    void deleteUser(String userName);
+    void deleteUser(String username);
 
     /**
      *  Method that returns if the user_name is registrated.
      *
-     *  @param userName user_name to check if its registrated.
+     *  @param username user_name to check if its registrated.
      *  @return Returns true if the user_name is registered in the database.
      */
-    boolean userNameRegistered(String userName);
+    boolean usernameRegistered(String username) throws PersistanceException;
 
     /**
      *  Method that returns if the email is registrated.
@@ -42,25 +44,25 @@ public interface UserDAO {
      *  @param email email to check if its registrated.
      *  @return Returns true if the email is registered in the database.
      */
-    boolean emailRegistered(String email);
+    boolean emailRegistered(String email) throws PersistanceException;
 
 
     /**
      *  Method that returns if the email and password introduced are valid.
      *
-     *  @param userName userName form the user (already verifyed with userNameRegistered).
+     *  @param username username form the user (already verifyed with usernameRegistered).
      *  @param password password to check.
-     *  @return Returns true if the userName,password are correct.
+     *  @return Returns true if the username,password are correct.
      */
-    boolean verifyPassword(String userName, String password);
+    boolean verifyPassword(String username, String password) throws PersistanceException;
 
     /**
      *  Method that returns the user from the database (class User).
      *
-     *  @param userName userName form the user to access.
+     *  @param username username form the user to access.
      *  @return User from the database with user_name introduced.
      */
-    EntityUser getUserFromUserName(String userName);
+    EntityUser getUserFromusername(String username) throws PersistanceException;;
 }
 
 
