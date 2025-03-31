b87525c21f6995baa895f5b9e9f553fd45606efb
Raúl Corominas <raul.corominas@students.salle.url.edu>
2025-03-31 20:25:03 +0200

    Updated views

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,7 @@
-import Presentation.Controllers.ControllerLogOut;
-import Presentation.Controllers.ControllerLogin;
-import Presentation.Controllers.ControllerRegister;
-import Presentation.Views.LogOutView;
-import Presentation.Views.LoginView;
-import Presentation.Views.RegisterView;
+import Presentation.Views.CoffeeClickerApp;
 
 public class Main {
     public static void main(String[] args) {
-        LogOutView logOutView = new LogOutView();
-        LoginView logInView = new LoginView();
-        RegisterView registerView = new RegisterView();
-
-        ControllerLogin controllerLogIn = new ControllerLogin(logInView);
-        logInView.controllerLogIn(controllerLogIn);
-
-        ControllerRegister controllerRegister = new ControllerRegister(registerView);
-        registerView.controllerRegister(controllerRegister);
-
-        ControllerLogOut controllerLogOut = new ControllerLogOut(logOutView);
-        logOutView.controllerLogOut(controllerLogOut);
-
+        CoffeeClickerApp app = new CoffeeClickerApp();
     }
 }
--- src/Presentation/Views/RegisterView.java
+++ src/Presentation/Views/RegisterView.java
@@ -1,60 +1,209 @@
 package Presentation.Views;
 
-import java.awt.event.ActionListener;
+import Presentation.Controllers.ControllerRegister;
 
-public class RegisterView {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.HashMap;
 
+public class RegisterView extends JPanel {
     public static final String REGISTER_COMMAND = "REGISTER_COMMAND";
+    public static final String BACK_COMMAND = "BACK_COMMAND";
+    private final CoffeeClickerApp app;
+    private final HashMap<String, JTextField> textFields = new HashMap<>();
+    private static final HashMap<String, JLabel> errorLabels = new HashMap<>();
+    private final HashMap<String, JButton> buttons = new HashMap<>();
 
-    public void showErrorPasswordConfirmationMessage(){
-        //TODO que muestre error por pantalla
+
+    public RegisterView(CoffeeClickerApp app) {
+        this.app = app;
+        setLayout(new BorderLayout());
+
+        ImageIcon headerIcon = new ImageIcon(new ImageIcon("imgs/header3.png")
+                .getImage().getScaledInstance(765, 171, Image.SCALE_SMOOTH));
+        JLabel label = new JLabel(headerIcon);
+        label.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        JPanel centerPanel = new JPanel(new GridBagLayout());
+
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.anchor = GridBagConstraints.CENTER;
+        centerPanel.add(mainPanel, gbc);
+
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.X_AXIS));
+
+        mainPanel.add(label);
+        mainPanel.add(Box.createVerticalStrut(50));
+        JPanel askUserPanel = askInfo("USER");
+        askUserPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        mainPanel.add(askUserPanel);
+        mainPanel.add(Box.createVerticalStrut(20));
+        JPanel askEmailPanel = askInfo("EMAIL");
+        askEmailPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        mainPanel.add(askEmailPanel);
+        mainPanel.add(Box.createVerticalStrut(20));
+        JPanel askPasswordPanel = askInfo("PASSWORD");
+        askPasswordPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        mainPanel.add(askPasswordPanel);
+        mainPanel.add(Box.createVerticalStrut(20));
+        JPanel askConfirmationPanel = askInfo("CONFIRM PASSWORD");
+        askConfirmationPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        mainPanel.add(askConfirmationPanel);
+        mainPanel.add(Box.createVerticalStrut(50));
+        buttonPanel.add(newButton("BACK", BACK_COMMAND));
+        buttonPanel.add(newButton("REGISTER", REGISTER_COMMAND));
+
+        mainPanel.add(buttonPanel);
+
+        add(centerPanel, BorderLayout.CENTER);
     }
 
-    public void showErrorEmailFormatMessage(){
-        //TODO que muestre error por pantalla
+    public JPanel askInfo(String text) {
+        JPanel infoBox = new JPanel();
+        infoBox.setLayout(new BoxLayout(infoBox, BoxLayout.Y_AXIS));
+        infoBox.setPreferredSize(new Dimension(617, 140));
+        infoBox.setMaximumSize(new Dimension(617, 140));
+        infoBox.setAlignmentX(Component.LEFT_ALIGNMENT);
+
+        JLabel label = new JLabel(text + ":");
+        label.setFont(new Font("CoffeeClicker", Font.BOLD, 24));
+        label.setForeground(Color.black);
+        label.setAlignmentX(Component.LEFT_ALIGNMENT);
+
+        JPanel textBoxContainer = new JPanel();
+        textBoxContainer.setLayout(new BoxLayout(textBoxContainer, BoxLayout.Y_AXIS));
+        textBoxContainer.setPreferredSize(new Dimension(617, 85));
+        textBoxContainer.setMaximumSize(new Dimension(617, 85));
+        textBoxContainer.setAlignmentX(Component.LEFT_ALIGNMENT);
+        textBoxContainer.setOpaque(false);
+
+        JPanel textBox = addTextbox(text);
+        textBox.setAlignmentX(Component.LEFT_ALIGNMENT);
+        textBoxContainer.add(textBox);
+
+        JLabel errorLabel = new JLabel(" ");
+        errorLabel.setForeground(Color.RED);
+        errorLabel.setFont(new Font("CoffeeClicker", Font.BOLD, 10));
+        errorLabel.setAlignmentX(Component.LEFT_ALIGNMENT);
+        errorLabel.setVerticalAlignment(SwingConstants.TOP);
+        errorLabel.setMaximumSize(new Dimension(600, 20));
+        errorLabel.setBorder(BorderFactory.createEmptyBorder(5, 5, 0, 0));
+
+        errorLabels.put(text, errorLabel);
+
+        infoBox.add(label);
+        infoBox.add(textBoxContainer);
+        infoBox.add(errorLabel);
+
+        return infoBox;
     }
 
-    public void showErrorExistingEmailMessage(){
-        //TODO que muestre error por pantalla
+    public JPanel addTextbox(String text) {
+        JPanel frame = new JPanel();
+
+        ImageIcon textbox_img = new ImageIcon(new ImageIcon("imgs/textBox.png")
+                .getImage().getScaledInstance(617, 75, Image.SCALE_SMOOTH));
+
+        BackgroundPanel panel = new BackgroundPanel(textbox_img);
+        panel.setLayout(new GridBagLayout());
+        panel.setPreferredSize(new Dimension(617, 75));
+        panel.setMaximumSize(new Dimension(617, 75));
+        panel.setOpaque(false);
+
+        JTextField textField = new JTextField(20);
+        textField.setOpaque(false);
+        textField.setBorder(BorderFactory.createEmptyBorder());
+        textField.setForeground(new Color(127, 51, 0));
+        textField.setFont(new Font("CoffeeClicker", Font.BOLD, 24));
+        textField.setPreferredSize(new Dimension(600, 70));
+
+        textFields.put(text, textField);
+
+        JPanel textFieldPanel = new JPanel(new GridBagLayout());
+        textFieldPanel.setOpaque(false);
+        textFieldPanel.add(textField);
+
+        panel.add(textFieldPanel);
+        frame.add(panel);
+        frame.setOpaque(false);
+        frame.setPreferredSize(new Dimension(617, 75));
+        return frame;
     }
 
-    public void showErrorExistingUserMessage(){
-        //TODO que muestre error por pantalla
+    private JButton newButton(String text, String actionCommand) {
+        ImageIcon buttonIcon = new ImageIcon(new ImageIcon("imgs/button.png")
+                .getImage().getScaledInstance(319, 102, Image.SCALE_DEFAULT));
+
+        ImageIcon buttonHoverIcon = new ImageIcon(new ImageIcon("imgs/button_selected.png")
+                .getImage().getScaledInstance(319, 102, Image.SCALE_DEFAULT));
+
+        JButton button = new JButton(text, buttonIcon);
+        button.setHorizontalTextPosition(JButton.CENTER);
+        button.setVerticalTextPosition(JButton.CENTER);
+        button.setFont(new Font("CoffeeClicker", Font.PLAIN, 36));
+        button.setBorderPainted(false);
+        button.setForeground(new Color(107, 41, 0));
+        button.setContentAreaFilled(false);
+        button.setFocusPainted(false);
+        button.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        button.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                button.setIcon(buttonHoverIcon);
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                button.setIcon(buttonIcon);
+            }
+        });
+
+        button.setActionCommand(actionCommand);
+
+        buttons.put(actionCommand, button);
+
+        return button;
     }
 
-    public void showSuccesfulLRegisterMessage(){
-        //TODO que muestre error por pantalla
+    public void clearFields() {
+        for (JTextField field : textFields.values()) {
+            field.setText(""); // Borra el texto de cada campo
+        }
     }
 
-    public String getUserText(){
-        //TODO pillar info del recuadro de texto de user
-        return "por rellenar";
+    public static void showError(String field, String message) {
+        JLabel errorLabel = errorLabels.get(field);
+        if (errorLabel != null) {
+            errorLabel.setText("<html>" + message.replaceAll("\n", "<br>") + "</html>");
+        }
     }
 
-    public String getEmailText(){
-        //TODO pillar info del recuadro de texto de user
-        return "por rellenar";
+    public void clearErrorMessages() {
+        for (JLabel label : errorLabels.values()) {
+            label.setText(" ");
+        }
     }
 
-    public String getPasswordText(){
-        //TODO pillar info del recuadro de texto de user
-        return "por rellenar";
+    public void setController(ControllerRegister controller) {
+        for (JButton button : buttons.values()) {
+            button.addActionListener(controller);
+        }
     }
 
-    public String getConfirmationPasswordText(){
-        //TODO pillar info del recuadro de texto de user
-        return "por rellenar";
+    public HashMap<String, JTextField> getTextFields() {
+        return textFields;
     }
 
-    public void showExceptionErrorMessage(){
-        //TODO informar de error al leer BBDD
+    public CoffeeClickerApp getApp() {
+        return app;
     }
-
-    public void controllerRegister(ActionListener actionListener) {
-        //Descomentar ->
-        //jbnLogOut.addActionListener(actionListener);
-        //jbnDeleteAccount.addActionListener(actionListener);
-        //jbnConfirmDeletion.setActionCommand(actionListener);
-    }
-
 }
